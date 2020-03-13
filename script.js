6031d62151155c3fbdeb58fabd22002faa65c63e
Jon <jong649@gmail.com>
2020-03-13 01:59:06 -0700

    save selected seats, movie, and ticket price to local storage

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
 const container = document.querySelector(".container");
-const seats = document.querySelectorAll(".row.seat:not(.occupied)");
+const seats = document.querySelectorAll(".row .seat:not(.occupied)");
 const count = document.getElementById("count");
 const total = document.getElementById("total");
 const movieSelect = document.getElementById("movie");
 let ticketPrice = +movieSelect.value;
 
+const setMovieData = (movieIndex, moviePrice) => {
+  localStorage.setItem("selectedMovieIndex", movieIndex);
+  localStorage.setItem("selectedMoviePrice", moviePrice);
+};
+
 const updateSelectedCount = () => {
   const selectedSeats = document.querySelectorAll(".row .seat.selected");
   const selectedSeatsCount = selectedSeats.length;
+
+  const seatsIndex = [...selectedSeats].map(seat => [...seats].indexOf(seat));
+
+  localStorage.setItem("selectedSeats", JSON.stringify(seatsIndex));
 
   count.innerText = selectedSeatsCount;
   total.innerText = selectedSeatsCount * ticketPrice;
@@ -15,6 +24,7 @@ const updateSelectedCount = () => {
 
 movieSelect.addEventListener("change", e => {
   ticketPrice = +e.target.value;
+  setMovieData();
   updateSelectedCount();
 });
 
